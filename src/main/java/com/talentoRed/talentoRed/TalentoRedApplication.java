b3e524e8cd9cd9a580a8d183ff6921f58767a097
Santiago <santiagodeazcuenaga2001@gmail.com>
2023-08-10 20:07:54 -0300

    agrego primer commit

---
+++ src/main/java/com/talentoRed/talentoRed/TalentoRedApplication.java
@@ -0,0 +1,13 @@
+package com.talentoRed.talentoRed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalentoRedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TalentoRedApplication.class, args);
+	}
+
+}
