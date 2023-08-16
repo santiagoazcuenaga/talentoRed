55d2faa7c766d5e47a7e257154d77f6ab1bb1c10
Santiago <santiagodeazcuenaga2001@gmail.com>
2023-08-16 19:03:55 -0300

    agrego todo al master

--- src/main/java/com/talentoRed/talentoRed/controladores/PortalControlador.java
+++ src/main/java/com/talentoRed/talentoRed/controladores/PortalControlador.java
@@ -2,17 +2,18 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package controladores;
+package com.talentoRed.talentoRed.controladores;
 
 import javax.xml.bind.ValidationException;
-import myExceptions.MyException;
+import com.talentoRed.talentoRed.myExceptions.MyException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
-import servicios.servicioUsuario;
+import com.talentoRed.talentoRed.servicios.servicioUsuario;
+import org.springframework.web.bind.annotation.GetMapping;
 
 /**
  *
@@ -27,19 +28,21 @@ public class PortalControlador {
     @Autowired
     servicioUsuario serusa = new servicioUsuario();
     
+    
+    @GetMapping("/")
     public String index(){
         return "index.html";
     }
-    
-    @PostMapping("/registrar") // Cambiado a POST en lugar de GET
-    public String registrar(@RequestParam("nombre") String nombre,
-            @RequestParam("email") String email,
-            @RequestParam("password") String password,
-            @RequestParam("direccion") String direccion) {
+       @GetMapping("/registrar")
+   public String registrar(){
+       return "registro.html";
+   }
+    @PostMapping("/registro") // Cambiado a POST en lugar de GET
+    public String registrar(@RequestParam String nombre,@RequestParam String email,@RequestParam String password,String direccion) {
         try {
             serusa.crearUsuario(nombre, email, password, direccion);
             // Registro exitoso, redirigir a la página de inicio de sesión
-            return "redirect:/login";
+            return "redirect:/";
         } catch (MyException e) {
             // Error durante el registro, mostrar mensaje de error en la página de registro
             // Puedes agregar el mensaje de error a través del Model y mostrarlo en la plantilla
