ec45fc25a351a195d9be09e9c2e6b12b8ea7ba35
Lukaku <lucascarballo8819@gmail.com>
2023-08-27 22:10:48 -0300

    panel admin

--- src/main/java/com/talentoRed/talentoRed/SeguridadWeb.java
+++ src/main/java/com/talentoRed/talentoRed/SeguridadWeb.java
@@ -38,7 +38,7 @@ public class SeguridadWeb extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/admin/*").hasRole("ADMIN")
+                .antMatchers("/admin/**").hasRole("ADMIN")
                 .antMatchers("/CSS/*", "/js/*", "/image/*", "/**")
                 .permitAll()
             .and().formLogin()
