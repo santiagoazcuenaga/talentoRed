1796f1dd5ec709aaa71ea11a517d4fae69ebd670
Lukaku <lucascarballo8819@gmail.com>
2023-08-31 19:46:00 -0300

    actualiza imagenes inicio

--- src/main/java/com/talentoRed/talentoRed/controladores/ImagenControlador.java
+++ src/main/java/com/talentoRed/talentoRed/controladores/ImagenControlador.java
@@ -1,8 +1,10 @@
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package com.talentoRed.talentoRed.controladores;
+
 
 import com.talentoRed.talentoRed.entidades.Usuario;
 import com.talentoRed.talentoRed.servicios.ServicioUsuario;
@@ -14,24 +16,22 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-/**
- *
- * @author Usuario
- */
+
+
 public class ImagenControlador {
-     @Autowired
- ServicioUsuario usuarioServicio;
- 
- @GetMapping("/perfil/{id}")
- public ResponseEntity<byte[]> imagenUsuario(@PathVariable String id){
-     Usuario usuario =  usuarioServicio.getOne(id);
-       
-    byte [] imagen = usuario.getImagen().getContenido();
-    
-   HttpHeaders headers = new HttpHeaders();  // dice que es de tipo imagen a la peticion
-    headers.setContentType(MediaType.IMAGE_JPEG);
-    
-     return new ResponseEntity<>(imagen,headers,HttpStatus.OK);  
+
+    @Autowired
+    ServicioUsuario usuarioServicio;
+
+    @GetMapping("/perfil/{id}")
+    public ResponseEntity<byte[]> imagenUsuario(@PathVariable String id) {
+        Usuario usuario = usuarioServicio.getOne(id);
+
+        byte[] imagen = usuario.getImagen().getContenido();
+
+        HttpHeaders headers = new HttpHeaders();  // dice que es de tipo imagen a la peticion
+        headers.setContentType(MediaType.IMAGE_JPEG);
+
+        return new ResponseEntity<>(imagen, headers, HttpStatus.OK);
     }
 }
-
