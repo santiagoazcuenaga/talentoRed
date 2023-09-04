e9e4eca58dd038e71cef6d26be23ed2727d23976
Santiago <santiagodeazcuenaga2001@gmail.com>
2023-09-03 23:50:52 -0300

    agrego lista completa

--- src/main/java/com/talentoRed/talentoRed/enums/TipoServicio.java
+++ src/main/java/com/talentoRed/talentoRed/enums/TipoServicio.java
@@ -6,5 +6,12 @@ package com.talentoRed.talentoRed.enums;
  * @author Kidver
  */
 public enum TipoServicio {
-    PLOMERO, ELECTRICISTA, GASISTA, ALBAÑIL, JARDINERO, MECANICO, CARPINTERO, TECNICO;
+   ALBAÑIL,
+CARPINTERO,
+ELECTRICISTA,
+GASISTA,
+JARDINERO,
+MECANICO,
+PLOMERO,
+TECNICO,
 }
